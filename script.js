5bb3aff3d32f23fa3e57d6e757eb3397523e4744
Caio Padilha <caiopadilha@id.uff.br>
2025-02-08 22:44:13 -0300

    Pontuação

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ window.onload = () => {
   const audioWinner = document.getElementById('audioWinner')
   const cars = document.getElementsByClassName('car')
   const alternatives = document.getElementsByClassName('playersImages')
+  const p1Score = document.getElementById('p1Score')
+  const p2Score = document.getElementById('p2Score')
 
   player1.style.marginLeft = 0
   player2.style.marginLeft = 0
+
+  if (localStorage.length === 0) {
+    p1Score.innerText = 0
+    p2Score.innerText = 0
+  } else if (localStorage.getItem('scorePlayer1') === null) {
+    p1Score.innerText = 0
+    p2Score.innerText = localStorage.getItem('scorePlayer2')
+  } else if (localStorage.getItem('scorePlayer2') === null) {
+    p2Score.innerText = 0
+    p1Score.innerText = localStorage.getItem('scorePlayer1')
+  } else {
+    p1Score.innerText = localStorage.getItem('scorePlayer1')
+    p2Score.innerText = localStorage.getItem('scorePlayer2')
+  }
 
   startBtn.addEventListener('click', event => {
     if (
@@ -25,12 +41,29 @@ window.onload = () => {
 
       if (player1Win) {
         alert('Player 1 wins')
+        if (localStorage.getItem('scorePlayer1') === null) {
+          localStorage.setItem('scorePlayer1', 1)
+          p1Score.innerText = localStorage.getItem('scorePlayer1')
+        } else {
+          let player1Score = parseInt(localStorage.getItem('scorePlayer1'))
+          localStorage.setItem('scorePlayer1', (player1Score += 1))
+          p1Score.innerText = localStorage.getItem('scorePlayer1')
+        }
+
         resetCars()
-        audioWinner.play()
+        // audioWinner.play()
       } else if (player2Win) {
         alert('Player 2 wins')
+        if (localStorage.getItem('scorePlayer2') === null) {
+          localStorage.setItem('scorePlayer2', 1)
+          p2Score.innerText = localStorage.getItem('scorePlayer2')
+        } else {
+          let player2Score = parseInt(localStorage.getItem('scorePlayer2'))
+          localStorage.setItem('scorePlayer2', (player2Score += 1))
+          p2Score.innerText = localStorage.getItem('scorePlayer2')
+        }
         resetCars()
-        audioWinner.play()
+        // audioWinner.play()
       }
     } else {
       alert('Você precisa selecionar os players.')
@@ -45,7 +78,11 @@ window.onload = () => {
     player2.style.backgroundImage = `url(./files/selectPlayer.png)`
   }
 
-  resetBtn.addEventListener('click', resetCars)
+  resetBtn.addEventListener('click', () => {
+    localStorage.clear()
+    window.location.reload()
+    // resetCars
+  })
 
   for (let index = 0; index < cars.length; index++) {
     cars[index].addEventListener('click', event => {
